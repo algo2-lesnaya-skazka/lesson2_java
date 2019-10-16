a0c1493932091bfdfd4152a6fe081c9d1a321ace
Stepanov Maxim <maksim_stepanov_06@mail.ru>
2019-10-16 18:18:02 +0300

    Add 1.3

---
+++ 1.3/src/wan.java
@@ -0,0 +1,15 @@
+
+public class wan {
+	public static void main(String[] args) {
+	       int name = 13;
+	       int age = name + 12;
+	       int city = age + 13;
+	       System.out.println(city);
+	   
+	}
+	
+}
+
+
+
+
