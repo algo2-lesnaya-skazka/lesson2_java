12a49fb031ab1871c83d5e2f209e152331b28c77
Shasha Lashch <lash.sasha@bk.ru>
2019-10-16 20:04:22 +0300

    Add lesson2_java files

---
+++ lesson2/src/lesson2/lesson2.java
@@ -0,0 +1,18 @@
+package lesson2;
+
+public class lesson2 {
+
+public static void main(String[] args) {
+
+int a = 3;
+int b = 2;
+
+
+System.out.print(b);
+System.out.print(" плюс ");
+System.out.print(a);
+System.out.print(" равно ");        
+System.out.print(a + b);
+ }
+
+}
