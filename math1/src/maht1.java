80a2ac3b55288f7f37a7e645c8e40439afd8bf3b
Rion Shekhovtsov <rion01045@gmail.com>
2019-10-16 18:57:28 +0300

    Add math1

---
+++ math1/src/maht1.java
@@ -0,0 +1,22 @@
+ public class maht1 {
+   public static void main(String[] args ) { 
+	    int q = 1;
+	    int x = 5;
+	    int g = q + x;
+	    int f = g * x;
+	    int d = f - q;
+	    int m = f * d;
+	    double k = 0.2;
+	    double l = m % k;
+	    System.out.println(g);
+	    System.out.println(f);
+	    System.out.println(q);
+	    System.out.println(x);
+	    System.out.println(d);
+	    System.out.println(m);
+	    System.out.println(l);
+	    System.out.println(k);
+	   
+	    
+	}
+ }
