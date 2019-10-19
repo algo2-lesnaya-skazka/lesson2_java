d8ac56395bcfa12b28632fea244bd37c97d17009
StepanovMaxim123 <49811239+StepanovMaxim123@users.noreply.github.com>
2019-10-19 20:52:44 +0300

    Add iz ISO-8859-1 v UTF-8

--- Введи число/src/wan.java
+++ Введи число/src/wan.java
@@ -2,9 +2,9 @@
 public class wan {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Введи любое целое число: ");
+		System.out.print("Р’РІРµРґРё Р»СЋР±РѕРµ С†РµР»РѕРµ С‡РёСЃР»Рѕ: ");
         long text = in.nextInt();   
-        System.out.print("Вы ввели число: ");
+        System.out.print("Р’С‹ РІРІРµР»Рё С‡РёСЃР»Рѕ: ");
         System.out.print(text);
         in.close();
 	}
