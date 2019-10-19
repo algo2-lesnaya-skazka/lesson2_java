dd82770fbae6c115a1e5b898c309632ea7138f76
Stepanov Maxim <maksim_stepanov_06@mail.ru>
2019-10-19 20:44:42 +0300

    Add code

---
+++ Введи число/src/wan.java
@@ -0,0 +1,16 @@
+ import java.util.Scanner;
+public class wan {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Введи любое целое число: ");
+        long text = in.nextInt();   
+        System.out.print("Вы ввели число: ");
+        System.out.print(text);
+        in.close();
+	}
+	
+}
+
+
+
+
